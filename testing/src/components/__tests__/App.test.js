8a95d3fd69185887fe650fbd997a30fb642871af
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2020-08-16 09:58:42 +0530

    26 compl

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../App";
+import { shallow } from "enzyme";
 
-it ("shows a comment box", () => {
+//using absolute path for imports, not relative.
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
+//enzyme way
+it("shows a comment box", () => {
+    const appComponent = shallow (<App />);
+    expect (appComponent.find(CommentBox).length).toEqual(1);
+})
+
+it("shows a comment list", () => {
+    const appComponent = shallow (<App />);
+    expect (appComponent.find(CommentList).length).toEqual(1);
+})
+///////////////////////////////
+
+//vanila way..
+it ("shows a comment box - vanilla way", () => {
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
     expect(div.innerHTML).toContain("Comment Box");
