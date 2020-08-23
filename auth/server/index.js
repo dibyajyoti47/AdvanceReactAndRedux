058f84814dd41d6c387a2708ce09b0b3c3659b93
dibyajyoti47 <dibyajyoti.6025@gmail.com>
2020-08-23 14:44:12 +0530

    course completed

--- auth/server/index.js
+++ auth/server/index.js
@@ -6,14 +6,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:27017/auth_react');
 
 // App Setup
 app.use(morgan('combined'));
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
